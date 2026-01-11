b465054eabd9cf727c207bad65f6e92748cae3be
Rain Veskus <veskus@proton.me>
2026-01-11 12:38:57 +0200

    fixed code

--- osa3/puhelinluettelobackend/index.js
+++ osa3/puhelinluettelobackend/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('data', (req,res) => JSON.stringify(req.body))
 
@@ -89,7 +90,7 @@ app.post('/api/persons', (req, res) => {
 })
 
 
-const PORT = process.envPORT || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
