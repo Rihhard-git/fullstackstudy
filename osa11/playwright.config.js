27da8add0141e80c543de83b639f746951749a1d
Rain Veskus <veskus@proton.me>
2026-04-07 00:36:50 +0300

    fixed e2e test

--- osa11/playwright.config.js
+++ osa11/playwright.config.js
@@ -75,7 +75,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
    webServer: {
      command: 'npm run start',
-     url: 'http://localhost:',
+     url: 'http://localhost:8080',
      reuseExistingServer: !process.env.CI,
    },
 });
