6de42aa1932c50c8cd335ddd8022182c20cbf3a3
Rain Veskus <veskus@proton.me>
2026-04-07 00:50:18 +0300

    added new test

--- osa11/tests/example.spec.js
+++ osa11/tests/example.spec.js
@@ -6,4 +6,12 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+  test('a certain pokemon can be viewed', async ({ page }) => {
+    
+    await page.goto('/pokemon/bulbasaur')
+    await expect(page.getByText('bulbasaur')).toBeVisible()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+    await expect(page.getByText('overgrow')).toBeVisible()
+
+  })
 })
